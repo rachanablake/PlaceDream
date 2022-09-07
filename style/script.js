0da0954250df6c41d3bd7720504abb186f40a4a8
Rachana Blake <rachanasor222@gmail.com>
2022-09-08 00:13:20 +0700

    Updae Navigation(Menu Link,Hide Bar), Colors, Border

---
+++ style/script.js
@@ -0,0 +1,27 @@
+// Scroll Drop Navigation-----------------------------------------------------------------------------
+// window.onscroll = function (e) {
+//     if (window.pageYOffset > 150) {
+//         document.querySelector('#header-scrolled').classList.add('navigation-blur');
+//     }
+//     else {
+//         document.querySelector('#header-scrolled').classList.remove('navigation-blur');
+//     }
+// }
+
+// Scroll Hide Navigation-----------------------------------------------------------------------
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header-scrolled").style.top = "0";
+  } else {
+    document.getElementById("header-scrolled").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// Dark Mode---------------------------------------------------------------------------------------
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
